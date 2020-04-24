b3323489dec25da1b492f7f3390ce9476c13c898
Hunter Stevens <hunterstevens1996@gmail.com>
2020-04-24 09:39:50 -0600

    Made Login Form with HandeChange and loginState

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,56 @@
-import React from "react";
+import React, {useState} from "react";
+import AxiosAuth from '../Utils/AxiosAuth';
+import {useParams, useHistory} from 'react-router-dom';
+
+
 
 const Login = () => {
+  const history = useHistory();
+
+  const initialState = {
+  username:"",
+  password:""
+}
+  const [loginState, setLoginState] = useState(initialState);
+
+  const handleChange = ev =>{
+    setLoginState({
+      ...loginState,
+      [ev.target.name]:ev.target.value
+    })
+  }
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const loginSubmit = ev =>{
+    ev.preventDefault();
+
+    AxiosAuth().post('')
+  }
+  
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={loginSubmit}>
+        <label htmlFor="username">
+          <input label="username"
+          type="text"
+          name="username"
+          value={loginState.username}
+          onChange={handleChange}
+          placeholder="username"
+          />
+        </label>
+
+        <label htmlFor="password">
+          <input label="password"
+          type="text"
+          name="password"
+          value={loginState.password}
+          onChange={handleChange}
+          placeholder="Password"
+          />
+        </label>
+      </form>
     </>
   );
 };
